830097cbf8ff78ff4c2de62a33f071a7debb1063
majlo <milos.kukla@gmail.com>
2013-06-04 23:43:15 +0200

    First functional version - now is functionality the same as in original TreeManager

--- TreeManager/lib/TreeManager.pm
+++ TreeManager/lib/TreeManager.pm
@@ -1,20 +1,32 @@
 package TreeManager;
 use Dancer ':syntax';
 use Dancer::Plugin::FlashMessage;
+use Tree;
 use TreeStorage;
+use Data::Dumper;
 
 our $VERSION = '0.1';
 
 get '/' => sub {
     my $storage = TreeStorage->new();
     my $names = $storage->getAllTreeNames();
-    my $activeTree = $storage->getActiveTree( config->{user_id} );
-    template 'index', { treeNames => $names, activeTree => $activeTree };
+    my $tree;
+    if (session->{activeTree}) {
+        $tree = Tree->new(session->{activeTree});
+        $tree->loadFromStorage();
+    }
+
+    template 'index', {
+        treeNames => $names,
+        activeTree => session->{activeTree},
+        tree => $tree && !$tree->isEmpty() ? $tree->toHtml() : '',
+    };
 };
 
 get '/selectTree' => sub {
     if (params->{selectedTree}) {
         TreeStorage->new()->setActiveTree( config->{user_id}, params->{selectedTree} );
+        session activeTree => params->{selectedTree};
     }
     redirect '/';
 };
@@ -23,14 +35,50 @@ post '/createTree' => sub {
     my $storage = TreeStorage->new();
     if (length (params->{treeName}) > 0) {
         if ($storage->existsTreeName(params->{treeName})) {
-            flash error => "Tree name already exists";
+            flash errorTreeCreate => "Tree name already exists";
         } else {
             $storage->saveNewTreeName(params->{treeName}, config->{user_id});
         }
     } else {
-        flash error => "Tree name cannot be empty";
+        flash errorTreeCreate => "Tree name cannot be empty";
     }
     
+    redirect '/';
+};
+
+post '/createRootNode' => sub {
+    Tree->new(session->{activeTree})->addRoot() if defined session->{activeTree};
+    redirect '/';
+};
+
+get '/addNode/:nodeId' => sub {
+    redirect '/' unless defined session->{activeTree};
+    my $tree = Tree->new(session->{activeTree});
+    if (
+        params->{addNode} &&
+        params->{addNode} =~ /^\d+$/ &&
+        defined $tree->getNode(params->{nodeId})
+    ) {
+        $tree->addNode(params->{nodeId});
+    } else {
+        flash errorAddNode => "Node ID not found";
+    }
+    redirect '/';
+};
+
+post '/addNode' => sub {
+    redirect '/' unless defined session->{activeTree};
+    my $tree = Tree->new(session->{activeTree});
+    $tree->loadFromStorage();
+    if (
+        params->{nodeId} &&
+        params->{nodeId} =~ /^\d+$/ &&
+        defined $tree->getNode(params->{nodeId})
+    ) {
+        $tree->addNode(params->{nodeId});
+    } else {
+        flash errorAddNode => "Node ID not found";
+    }
     redirect '/';
 };
 
